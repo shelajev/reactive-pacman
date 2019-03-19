47bdd6bc2f80f707696b606eea3518872e75ff02
Oleh Dokuka <shadowgun@i.ua>
2019-03-19 17:46:10 +0200

    removes unused file

--- scripts/copy-folder.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-// destination.txt will be created or overwritten by default.
-fs.copyF(process.argv[2], process.argv[3], (err) => {
-  if (err) throw err;
-  console.log(`${process.argv[2]} was copied to ${process.argv[3]}`);
-});
